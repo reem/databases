e317ebc21d6d31038ae23e9e98daa66054bbe99f
Hack Reactor Students <jonathan.reem@gmail.com>
2014-05-10 11:18:11 -0700

    Factored out dbConnection into its own module.

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,9 +1,15 @@
+/* globals require, dbConnection, module */
+
 var mysql = require("mysql");
 
-dbConnection = mysql.createConnection({
-  user: "root",
-  password: "",
-  database: "chat"
-});
+module.exports = (function () {
+  dbConnection = mysql.createConnection({
+    user: "root",
+    password: "",
+    database: "chat"
+  });
 
-dbConnection.connect();
+  dbConnection.connect();
+
+  return dbConnection;
+}());
