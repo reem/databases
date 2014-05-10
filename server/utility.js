f1484982cc8841641e48d1d73b27b1e9a6237216
Hack Reactor Students <jonathan.reem@gmail.com>
2014-05-10 11:18:25 -0700

    Removed broken example code.

--- server/utility.js
+++ server/utility.js
@@ -13,33 +13,3 @@ module.exports.failable = function (failer, error, success) {
 module.exports.promise = function (func, defer) {
   module.exports.failable(func, defer.reject, defer.resolve);
 };
-
-var promiseRead = function (path) {
-  var defer = Q.defer();
-  util.promise(_.partial(fs.readFile, path));
-  return defer;
-};
-
-var promiseRead = function (path) {
-  var defer = Q.defer();
-
-  fs.readFile(path, function (err, data) {
-    if (err) {
-      defer.reject(err);
-    } else {
-      defer.resolve(data);
-    }
-  });
-
-  return defer;
-};
-
-var filePromise = promiseRead("hello.txt");
-filePromise
-  .then(function (contentes) {
-    //
-  })
-  .fail(function (err) {
-    console.error(err);
-  });
-
