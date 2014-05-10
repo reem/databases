61b900bf1ff266b670e56c152f2790127c918e33
Hack Reactor Students <jonathan.reem@gmail.com>
2014-05-10 11:19:18 -0700

    Added hard-coded SQL queries.

--- server/models.js
+++ server/models.js
@@ -1,8 +1,9 @@
-/* globals dbConnection, require, module */
+/* globals require, module */
 
 var _ = require("underscore");
 var Q = require("q");
 var util = require("./utility.js");
+var dbConnection = require("./dbConnection.js");
 
 var sqlToPromise = function (queryString) {
   return function (queryArguments) {
@@ -18,14 +19,22 @@ var sqlToPromise = function (queryString) {
 };
 
 module.exports = {
-  addRoom : sqlToPromise(""),
-  getRooms : sqlToPromise(""),
-  addMessage : sqlToPromise("INSERT INTO Users(username) VALUES (?);" +
-                            "INSERT INTO Rooms(roomname) VALUES (?) " +
-                            "INSERT INTO Messages(text, userid, roomid) " +
-                            "VALUES (?, (SELECT id FROM Users WHERE username = ?), "+
-                            "  (SELECT id FROM Rooms WHERE roomname = ?));"),
-  getMessages : sqlToPromise(""),
-  addUser : sqlToPromise("")
+  addRoom : sqlToPromise("INSERT INTO Rooms(roomname) " +
+                         "VALUES (?);"),
+  getRooms : sqlToPromise("SELECT roomname FROM Rooms"),
+
+  addMessage : sqlToPromise("INSERT INTO Messages(text, userid, roomid) " +
+                            "VALUES (?, (SELECT id FROM Users WHERE username = ? LIMIT 1), "+
+                            "  (SELECT id FROM Rooms WHERE roomname = ? LIMIT 1));"),
+  getMessages : sqlToPromise("SELECT u.username AS username, m.text AS text, r.roomname AS roomname " +
+                             "FROM Messages m " +
+                             "INNER JOIN Users u " +
+                             "ON m.userid = u.id " +
+                             "INNER JOIN Rooms r " +
+                             "ON m.roomid = r.id;"),
+
+  addUser : sqlToPromise("INSERT INTO Users(username) " +
+                         "VALUES (?);"),
+  getUsers : sqlToPromise("SELECT username FROM Users")
 };
 
